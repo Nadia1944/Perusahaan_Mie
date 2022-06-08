16be9ad57c7e68d458ddbef4e25013c8d19d50f8
AnggisPangesti09 <anggisprasita@gmail.com>
2022-06-08 08:17:39 +0700

    Warehouse RM_ Anggis

---
+++ K.3 - Perusahaan Mie/K.3 - Perusahaan Mie/K.3 - Perusahaan Mie/Warehouse_RM.Designer.vb
@@ -0,0 +1,432 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class WarehouseRM
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btn_exit = New System.Windows.Forms.Button()
+        Me.btn_cancel = New System.Windows.Forms.Button()
+        Me.btn_delete = New System.Windows.Forms.Button()
+        Me.btn_edit = New System.Windows.Forms.Button()
+        Me.btn_save = New System.Windows.Forms.Button()
+        Me.btn_input = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtSatuan_RM = New System.Windows.Forms.TextBox()
+        Me.txtJumlah_Stok_RM = New System.Windows.Forms.TextBox()
+        Me.txtNama_RM = New System.Windows.Forms.TextBox()
+        Me.txtId_RM = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.txtSatuan_RM2 = New System.Windows.Forms.TextBox()
+        Me.txtJumlah_Stok_RM2 = New System.Windows.Forms.TextBox()
+        Me.txtNama_RM2 = New System.Windows.Forms.TextBox()
+        Me.txtId_RM2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btn_exit2 = New System.Windows.Forms.Button()
+        Me.btn_cancel2 = New System.Windows.Forms.Button()
+        Me.btn_delete2 = New System.Windows.Forms.Button()
+        Me.btn_edit2 = New System.Windows.Forms.Button()
+        Me.btn_save2 = New System.Windows.Forms.Button()
+        Me.btn_input2 = New System.Windows.Forms.Button()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.DataGridView1)
+        Me.GroupBox2.Location = New System.Drawing.Point(11, 282)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(2)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(2)
+        Me.GroupBox2.Size = New System.Drawing.Size(404, 184)
+        Me.GroupBox2.TabIndex = 39
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Detail Data Warehouse"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(4, 17)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(2)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 28
+        Me.DataGridView1.Size = New System.Drawing.Size(383, 156)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'btn_exit
+        '
+        Me.btn_exit.Location = New System.Drawing.Point(228, 245)
+        Me.btn_exit.Name = "btn_exit"
+        Me.btn_exit.Size = New System.Drawing.Size(75, 23)
+        Me.btn_exit.TabIndex = 38
+        Me.btn_exit.Text = "EXIT"
+        Me.btn_exit.UseVisualStyleBackColor = True
+        '
+        'btn_cancel
+        '
+        Me.btn_cancel.Location = New System.Drawing.Point(122, 245)
+        Me.btn_cancel.Name = "btn_cancel"
+        Me.btn_cancel.Size = New System.Drawing.Size(75, 23)
+        Me.btn_cancel.TabIndex = 37
+        Me.btn_cancel.Text = "CANCEL"
+        Me.btn_cancel.UseVisualStyleBackColor = True
+        '
+        'btn_delete
+        '
+        Me.btn_delete.Location = New System.Drawing.Point(20, 245)
+        Me.btn_delete.Name = "btn_delete"
+        Me.btn_delete.Size = New System.Drawing.Size(75, 23)
+        Me.btn_delete.TabIndex = 36
+        Me.btn_delete.Text = "DELETE"
+        Me.btn_delete.UseVisualStyleBackColor = True
+        '
+        'btn_edit
+        '
+        Me.btn_edit.Location = New System.Drawing.Point(228, 205)
+        Me.btn_edit.Name = "btn_edit"
+        Me.btn_edit.Size = New System.Drawing.Size(75, 23)
+        Me.btn_edit.TabIndex = 35
+        Me.btn_edit.Text = "EDIT"
+        Me.btn_edit.UseVisualStyleBackColor = True
+        '
+        'btn_save
+        '
+        Me.btn_save.Location = New System.Drawing.Point(122, 205)
+        Me.btn_save.Name = "btn_save"
+        Me.btn_save.Size = New System.Drawing.Size(75, 23)
+        Me.btn_save.TabIndex = 34
+        Me.btn_save.Text = "SAVE"
+        Me.btn_save.UseVisualStyleBackColor = True
+        '
+        'btn_input
+        '
+        Me.btn_input.Location = New System.Drawing.Point(20, 205)
+        Me.btn_input.Name = "btn_input"
+        Me.btn_input.Size = New System.Drawing.Size(75, 23)
+        Me.btn_input.TabIndex = 33
+        Me.btn_input.Text = "INPUT"
+        Me.btn_input.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtSatuan_RM)
+        Me.GroupBox1.Controls.Add(Me.txtJumlah_Stok_RM)
+        Me.GroupBox1.Controls.Add(Me.txtNama_RM)
+        Me.GroupBox1.Controls.Add(Me.txtId_RM)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Location = New System.Drawing.Point(11, 11)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(346, 176)
+        Me.GroupBox1.TabIndex = 32
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Input Data Warehouse"
+        '
+        'txtSatuan_RM
+        '
+        Me.txtSatuan_RM.Location = New System.Drawing.Point(165, 137)
+        Me.txtSatuan_RM.Name = "txtSatuan_RM"
+        Me.txtSatuan_RM.Size = New System.Drawing.Size(110, 20)
+        Me.txtSatuan_RM.TabIndex = 11
+        '
+        'txtJumlah_Stok_RM
+        '
+        Me.txtJumlah_Stok_RM.Location = New System.Drawing.Point(165, 101)
+        Me.txtJumlah_Stok_RM.Name = "txtJumlah_Stok_RM"
+        Me.txtJumlah_Stok_RM.Size = New System.Drawing.Size(110, 20)
+        Me.txtJumlah_Stok_RM.TabIndex = 10
+        '
+        'txtNama_RM
+        '
+        Me.txtNama_RM.Location = New System.Drawing.Point(165, 68)
+        Me.txtNama_RM.Name = "txtNama_RM"
+        Me.txtNama_RM.Size = New System.Drawing.Size(110, 20)
+        Me.txtNama_RM.TabIndex = 9
+        '
+        'txtId_RM
+        '
+        Me.txtId_RM.Location = New System.Drawing.Point(165, 32)
+        Me.txtId_RM.Name = "txtId_RM"
+        Me.txtId_RM.Size = New System.Drawing.Size(110, 20)
+        Me.txtId_RM.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(20, 144)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(64, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Satuan_RM"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(20, 104)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(91, 13)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Jumlah_Stok_RM"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(20, 71)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(58, 13)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Nama_RM"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(20, 39)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(39, 13)
+        Me.Label8.TabIndex = 4
+        Me.Label8.Text = "Id_RM"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.btn_exit2)
+        Me.GroupBox3.Controls.Add(Me.btn_cancel2)
+        Me.GroupBox3.Controls.Add(Me.btn_delete2)
+        Me.GroupBox3.Controls.Add(Me.btn_edit2)
+        Me.GroupBox3.Controls.Add(Me.btn_save2)
+        Me.GroupBox3.Controls.Add(Me.btn_input2)
+        Me.GroupBox3.Controls.Add(Me.txtSatuan_RM2)
+        Me.GroupBox3.Controls.Add(Me.txtJumlah_Stok_RM2)
+        Me.GroupBox3.Controls.Add(Me.txtNama_RM2)
+        Me.GroupBox3.Controls.Add(Me.txtId_RM2)
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.DataGridView2)
+        Me.GroupBox3.Location = New System.Drawing.Point(434, 25)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(492, 441)
+        Me.GroupBox3.TabIndex = 40
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Inventori RM"
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(12, 22)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(468, 180)
+        Me.DataGridView2.TabIndex = 0
+        '
+        'txtSatuan_RM2
+        '
+        Me.txtSatuan_RM2.Location = New System.Drawing.Point(168, 318)
+        Me.txtSatuan_RM2.Name = "txtSatuan_RM2"
+        Me.txtSatuan_RM2.Size = New System.Drawing.Size(110, 20)
+        Me.txtSatuan_RM2.TabIndex = 19
+        '
+        'txtJumlah_Stok_RM2
+        '
+        Me.txtJumlah_Stok_RM2.Location = New System.Drawing.Point(168, 282)
+        Me.txtJumlah_Stok_RM2.Name = "txtJumlah_Stok_RM2"
+        Me.txtJumlah_Stok_RM2.Size = New System.Drawing.Size(110, 20)
+        Me.txtJumlah_Stok_RM2.TabIndex = 18
+        '
+        'txtNama_RM2
+        '
+        Me.txtNama_RM2.Location = New System.Drawing.Point(168, 249)
+        Me.txtNama_RM2.Name = "txtNama_RM2"
+        Me.txtNama_RM2.Size = New System.Drawing.Size(110, 20)
+        Me.txtNama_RM2.TabIndex = 17
+        '
+        'txtId_RM2
+        '
+        Me.txtId_RM2.Location = New System.Drawing.Point(168, 213)
+        Me.txtId_RM2.Name = "txtId_RM2"
+        Me.txtId_RM2.Size = New System.Drawing.Size(110, 20)
+        Me.txtId_RM2.TabIndex = 16
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 325)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 13)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "Satuan_RM"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 285)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 13)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "Jumlah_Stok_RM"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 252)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Nama_RM"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(23, 220)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Id_RM"
+        '
+        'btn_exit2
+        '
+        Me.btn_exit2.Location = New System.Drawing.Point(234, 394)
+        Me.btn_exit2.Name = "btn_exit2"
+        Me.btn_exit2.Size = New System.Drawing.Size(75, 23)
+        Me.btn_exit2.TabIndex = 44
+        Me.btn_exit2.Text = "EXIT"
+        Me.btn_exit2.UseVisualStyleBackColor = True
+        '
+        'btn_cancel2
+        '
+        Me.btn_cancel2.Location = New System.Drawing.Point(128, 394)
+        Me.btn_cancel2.Name = "btn_cancel2"
+        Me.btn_cancel2.Size = New System.Drawing.Size(75, 23)
+        Me.btn_cancel2.TabIndex = 43
+        Me.btn_cancel2.Text = "CANCEL"
+        Me.btn_cancel2.UseVisualStyleBackColor = True
+        '
+        'btn_delete2
+        '
+        Me.btn_delete2.Location = New System.Drawing.Point(26, 394)
+        Me.btn_delete2.Name = "btn_delete2"
+        Me.btn_delete2.Size = New System.Drawing.Size(75, 23)
+        Me.btn_delete2.TabIndex = 42
+        Me.btn_delete2.Text = "DELETE"
+        Me.btn_delete2.UseVisualStyleBackColor = True
+        '
+        'btn_edit2
+        '
+        Me.btn_edit2.Location = New System.Drawing.Point(234, 354)
+        Me.btn_edit2.Name = "btn_edit2"
+        Me.btn_edit2.Size = New System.Drawing.Size(75, 23)
+        Me.btn_edit2.TabIndex = 41
+        Me.btn_edit2.Text = "EDIT"
+        Me.btn_edit2.UseVisualStyleBackColor = True
+        '
+        'btn_save2
+        '
+        Me.btn_save2.Location = New System.Drawing.Point(128, 354)
+        Me.btn_save2.Name = "btn_save2"
+        Me.btn_save2.Size = New System.Drawing.Size(75, 23)
+        Me.btn_save2.TabIndex = 40
+        Me.btn_save2.Text = "SAVE"
+        Me.btn_save2.UseVisualStyleBackColor = True
+        '
+        'btn_input2
+        '
+        Me.btn_input2.Location = New System.Drawing.Point(26, 354)
+        Me.btn_input2.Name = "btn_input2"
+        Me.btn_input2.Size = New System.Drawing.Size(75, 23)
+        Me.btn_input2.TabIndex = 39
+        Me.btn_input2.Text = "INPUT"
+        Me.btn_input2.UseVisualStyleBackColor = True
+        '
+        'WarehouseRM
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(938, 490)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.btn_exit)
+        Me.Controls.Add(Me.btn_cancel)
+        Me.Controls.Add(Me.btn_delete)
+        Me.Controls.Add(Me.btn_edit)
+        Me.Controls.Add(Me.btn_save)
+        Me.Controls.Add(Me.btn_input)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "WarehouseRM"
+        Me.Text = "WarehouseRM"
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btn_exit As Button
+    Friend WithEvents btn_cancel As Button
+    Friend WithEvents btn_delete As Button
+    Friend WithEvents btn_edit As Button
+    Friend WithEvents btn_save As Button
+    Friend WithEvents btn_input As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtSatuan_RM As TextBox
+    Friend WithEvents txtJumlah_Stok_RM As TextBox
+    Friend WithEvents txtNama_RM As TextBox
+    Friend WithEvents txtId_RM As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents btn_exit2 As Button
+    Friend WithEvents btn_cancel2 As Button
+    Friend WithEvents btn_delete2 As Button
+    Friend WithEvents btn_edit2 As Button
+    Friend WithEvents btn_save2 As Button
+    Friend WithEvents btn_input2 As Button
+    Friend WithEvents txtSatuan_RM2 As TextBox
+    Friend WithEvents txtJumlah_Stok_RM2 As TextBox
+    Friend WithEvents txtNama_RM2 As TextBox
+    Friend WithEvents txtId_RM2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents DataGridView2 As DataGridView
+End Class
---
+++ K.3 - Perusahaan Mie/K.3 - Perusahaan Mie/K.3 - Perusahaan Mie/Warehouse_RM.vb
@@ -0,0 +1,240 @@
+﻿Public Class WarehouseRM
+    Sub KosongkanForm1()
+        txtId_RM.Text = ""
+        txtNama_RM.Text = ""
+        txtJumlah_Stok_RM.Text = ""
+        txtSatuan_RM.Text = ""
+    End Sub
+    Sub MatikanForm1()
+        txtId_RM.Enabled = False
+        txtNama_RM.Enabled = False
+        txtJumlah_Stok_RM.Enabled = False
+        txtSatuan_RM.Enabled = False
+
+    End Sub
+    Sub HidupkanForm1()
+        txtId_RM.Enabled = True
+        txtNama_RM.Enabled = True
+        txtJumlah_Stok_RM.Enabled = True
+        txtSatuan_RM.Enabled = True
+
+    End Sub
+    Sub TampilkanData1()
+        Call koneksiDB()
+        DA = New OleDb.OleDbDataAdapter("select * from Warehouse_RM ", Conn)
+        DS = New DataSet
+        DA.Fill(DS)
+        DataGridView1.DataSource = DS.Tables(0)
+        DataGridView1.ReadOnly = True
+    End Sub
+    Private Sub Customer_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Call MatikanForm()
+        Call TampilkanData()
+    End Sub
+    Private Sub btn_input_Click(sender As Object, e As EventArgs) Handles btn_input.Click
+        Call HidupkanForm()
+        Call KosongkanForm()
+    End Sub
+
+    Private Sub btn_save_Click(sender As Object, e As EventArgs) Handles btn_save.Click
+        If txtId_RM.Text = "" Or txtNama_RM.Text = "" Or txtJumlah_Stok_RM.Text = "" Or txtSatuan_RM.Text = "" Then
+            MsgBox("Data Warehouse_RM Belum Lengkap")
+            Exit Sub
+        Else
+            Call koneksiDB()
+            CMD = New OleDb.OleDbCommand(" select * from Warehouse_RM where Id_RM='" & txtId_RM.Text & "'", Conn)
+            DM = CMD.ExecuteReader
+            DM.Read()
+            If Not DM.HasRows Then
+                Call koneksiDB()
+                Dim simpan As String
+                simpan = "insert into Warehouse_RM values ('" & txtId_RM.Text &
+               "', '" & txtNama_RM.Text & "',  '" & txtJumlah_Stok_RM.Text & "', '" & txtSatuan_RM.Text & "')"
+                CMD = New OleDb.OleDbCommand(simpan, Conn)
+                CMD.ExecuteNonQuery()
+                MsgBox("Input Data Sukses")
+            Else
+                MsgBox("Data Sudah Ada")
+            End If
+            Call MatikanForm()
+            Call KosongkanForm()
+            Call TampilkanData()
+        End If
+    End Sub
+
+    Private Sub btn_edit_Click(sender As Object, e As EventArgs) Handles btn_edit.Click
+        If txtId_RM.Text = "" Or txtNama_RM.Text = "" Or txtJumlah_Stok_RM.Text = "" Or txtSatuan_RM.Text = Then
+            MsgBox("Data Warehouse_RM Belum Lengkap")
+            Exit Sub
+        Else
+            Call koneksiDB()
+            CMD = New OleDb.OleDbCommand("update Warehouse_RM set Nama_RM = '" &
+           txtNama_RM.Text & "', Jumlah_Stok_RM = '" & txtJumlah_Stok_RM.Text & "', No_Hp = '" & txtSatuan_RM.Text & "' 
+           where Id_RM ='" & txtId_RM.Text & "'", Conn)
+            DM = CMD.ExecuteReader
+            MsgBox("Update Data Berhasil")
+        End If
+        Call KosongkanForm()
+        Call MatikanForm()
+        Call TampilkanData()
+    End Sub
+
+    Private Sub btn_delete_Click(sender As Object, e As EventArgs) Handles btn_delete.Click
+        If txtId_RM.Text = "" Then
+            MsgBox("Tidak ada data yang dipilih")
+            Exit Sub
+        Else
+            If MessageBox.Show(" Are you sure to delete this data?", "Konfirmasi", MessageBoxButtons.YesNoCancel) Then
+                Call koneksiDB()
+                CMD = New OleDb.OleDbCommand(" delete from Customer where Id_RM = '" & txtId_RM.Text & "'", Conn)
+                DM = CMD.ExecuteReader
+                MsgBox("Data Berhasil Dihapus")
+                Call MatikanForm()
+                Call KosongkanForm()
+                Call TampilkanData()
+            Else
+                Call KosongkanForm()
+                Call TampilkanData()
+            End If
+        End If
+    End Sub
+
+    Private Sub btn_cancel_Click(sender As Object, e As EventArgs) Handles btn_cancel.Click
+        Call MatikanForm()
+        Call KosongkanForm()
+    End Sub
+
+    Private Sub btn_exit_Click(sender As Object, e As EventArgs) Handles btn_exit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        On Error Resume Next
+        txtId_RM.Text = DataGridView1.Rows(e.RowIndex).Cells(0).Value
+        txtNama_RM.Text = DataGridView1.Rows(e.RowIndex).Cells(1).Value
+        txtJumlah_Stok_RM.Text = DataGridView1.Rows(e.RowIndex).Cells(2).Value
+        txtSatuan_RM.Text = DataGridView1.Rows(e.RowIndex).Cells(3).Value
+        Call HidupkanForm()
+        txtId_RM.Enabled = False
+    End Sub
+
+    Sub KosongkanForm()
+        txtId_RM2.Text = ""
+        txtNama_RM2.Text = ""
+        txtJumlah_Stok_RM2.Text = ""
+        txtSatuan_RM2.Text = ""
+    End Sub
+    Sub MatikanForm()
+        txtId_RM2.Enabled = False
+        txtNama_RM2.Enabled = False
+        txtJumlah_Stok_RM2.Enabled = False
+        txtSatuan_RM2.Enabled = False
+
+    End Sub
+    Sub HidupkanForm()
+        txtId_RM2.Enabled = True
+        txtNama_RM2.Enabled = True
+        txtJumlah_Stok_RM2.Enabled = True
+        txtSatuan_RM2.Enabled = True
+
+    End Sub
+    Sub TampilkanData()
+        Call koneksiDB()
+        DA = New OleDb.OleDbDataAdapter("select * from Warehouse_RM ", Conn)
+        DS = New DataSet
+        DA.Fill(DS)
+        DataGridView2.DataSource = DS.Tables(0)
+        DataGridView2.ReadOnly = True
+    End Sub
+    Private Sub Customer_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Call MatikanForm()
+        Call TampilkanData()
+    End Sub
+    Private Sub btn_input_Click(sender As Object, e As EventArgs) Handles btn_input2.Click
+        Call HidupkanForm()
+        Call KosongkanForm()
+    End Sub
+
+    Private Sub btn_save2_Click(sender As Object, e As EventArgs) Handles btn_save2.Click
+        If txtId_RM2.Text = "" Or txtNama_RM2.Text = "" Or txtJumlah_Stok_RM2.Text = "" Or txtSatuan_RM2.Text = "" Then
+            MsgBox("Data Warehouse_RM Belum Lengkap")
+            Exit Sub
+        Else
+            Call koneksiDB()
+            CMD = New OleDb.OleDbCommand(" select * from Warehouse_RM where Id_RM ='" & txtId_RM2.Text & "'", Conn)
+            DM = CMD.ExecuteReader
+            DM.Read()
+            If Not DM.HasRows Then
+                Call koneksiDB()
+                Dim simpan As String
+                simpan = "insert into Warehouse_RM values ('" & txtId_RM2.Text &
+               "', '" & txtNama_RM2.Text & "',  '" & txtJumlah_Stok_RM2.Text & "', '" & txtSatuan_RM2.Text & "')"
+                CMD = New OleDb.OleDbCommand(simpan, Conn)
+                CMD.ExecuteNonQuery()
+                MsgBox("Input Data Sukses")
+            Else
+                MsgBox("Data Sudah Ada")
+            End If
+            Call MatikanForm()
+            Call KosongkanForm()
+            Call TampilkanData()
+        End If
+    End Sub
+
+    Private Sub btn_edit2_Click(sender As Object, e As EventArgs) Handles btn_edit2.Click
+        If txtId_RM2.Text = "" Or txtNama_RM2.Text = "" Or txtJumlah_Stok_RM2.Text = "" Or txtSatuan_RM2.Text = Then
+            MsgBox("Data Warehouse_RM Belum Lengkap")
+            Exit Sub
+        Else
+            Call koneksiDB()
+            CMD = New OleDb.OleDbCommand("update Warehouse_RM set Nama_RM = '" &
+           txtNama_RM2.Text & "', Jumlah_Stok_RM = '" & txtJumlah_Stok_RM2.Text & "', No_Hp = '" & txtSatuan_RM2.Text & "' 
+           where Id_RM ='" & txtId_RM2.Text & "'", Conn)
+            DM = CMD.ExecuteReader
+            MsgBox("Update Data Berhasil")
+        End If
+        Call KosongkanForm()
+        Call MatikanForm()
+        Call TampilkanData()
+    End Sub
+
+    Private Sub btn_delete2_Click(sender As Object, e As EventArgs) Handles btn_delete2.Click
+        If txtId_RM2.Text = "" Then
+            MsgBox("Tidak ada data yang dipilih")
+            Exit Sub
+        Else
+            If MessageBox.Show(" Are you sure to delete this data?", "Konfirmasi", MessageBoxButtons.YesNoCancel) Then
+                Call koneksiDB()
+                CMD = New OleDb.OleDbCommand(" delete from Customer where Id_RM = '" & txtId_RM2.Text & "'", Conn)
+                DM = CMD.ExecuteReader
+                MsgBox("Data Berhasil Dihapus")
+                Call MatikanForm()
+                Call KosongkanForm()
+                Call TampilkanData()
+            Else
+                Call KosongkanForm()
+                Call TampilkanData()
+            End If
+        End If
+    End Sub
+
+    Private Sub btn_cancel2_Click(sender As Object, e As EventArgs) Handles btn_cancel2.Click
+        Call MatikanForm()
+        Call KosongkanForm()
+    End Sub
+
+    Private Sub btn_exit2_Click(sender As Object, e As EventArgs) Handles btn_exit2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub DataGridView2_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView2.CellContentClick
+        On Error Resume Next
+        txtId_RM2.Text = DataGridView2.Rows(e.RowIndex).Cells(0).Value
+        txtNama_RM2.Text = DataGridView2.Rows(e.RowIndex).Cells(1).Value
+        txtJumlah_Stok_RM2.Text = DataGridView2.Rows(e.RowIndex).Cells(2).Value
+        txtSatuan_RM.Text = DataGridView2.Rows(e.RowIndex).Cells(3).Value
+        Call HidupkanForm()
+        txtId_RM.Enabled = False
+    End Sub
+
+End Class
